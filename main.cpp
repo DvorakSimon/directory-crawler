e7accf5f2c8d83a9872c5b685c605b686c91e631
DvorakSimon <simond.dvorak@gmail.com>
2016-04-01 10:20:31 +0200

    Formální dodělávky
    Návrh komentářů, odstranění nepotřebných proměnných, odstranění testovacích příkazů a cyklů.

--- main.cpp
+++ main.cpp
@@ -2,34 +2,49 @@
 #include <vector>
 #include <stack>
 #include <dirent.h>
-#include <stdio.h>
 #include <string.h>
 
 using namespace std;
 
-vector<string> getFiles(string filePath)
+/*
+ * The function returns directories found in directory.
+ * First parameter is path where you want to find directories.
+ * You can also use double slashes like this ...../dir/dir//dir. Not reomanded!
+ */
+vector<string> getDirs(string dirPath)
 {
-    const char* DIR_PATH = filePath.c_str();
+    // Converting path to const char* because the function opendir is programmed in c
+    // and c methods parameters accepts only const char* so I must convert it.
+    const char* DIR_PATH = dirPath.c_str();
 
+    // Declaration of dynamic array that contains found directories by path.
     vector<string> directories;
 
+    // Call a function to try to open the dir by path.
+    // If it doesnt exists the function will return segmentation error.
     DIR *dir = opendir(DIR_PATH);
 
+    // Then try to read the dir
     struct dirent *entry = readdir(dir);
 
+    // While dir has a directory.
     while (entry != NULL)
     {
+        // If type of stream is directory.
         if (entry->d_type == DT_DIR) {
+            // If the "directory" name is not ".." or ".".
             if(!strcmp(entry->d_name, "..") || !strcmp(entry->d_name, ".")) {
 
             } else {
-                directories.push_back(filePath + "/" + entry->d_name);
+                // push the actual dir path PLUS actual directory to the dynamic array.
+                directories.push_back(dirPath + "/" + entry->d_name);
             }
         }
 
         entry = readdir(dir);
     }
 
+    // Close the dir.
     closedir(dir);
 
     return directories;
@@ -38,6 +53,7 @@ vector<string> getFiles(string filePath)
 
 int main(void)
 {
+    // A root path where you want to start searching.
     string searchedDir = "/home/simon/Plocha/Simon";
 
     stack<string> actDir;
@@ -48,8 +64,6 @@ int main(void)
 
     vector<string> dirFound;
 
-    string newString = searchedDir;
-
     int dirCount = 0;
 
     while(actDir.size() > 0) {
@@ -57,13 +71,11 @@ int main(void)
 
         cout << actSearchedDir << endl;
 
-        dirFound = getFiles(actSearchedDir.c_str());
+        dirFound = getDirs(actSearchedDir.c_str());
 
         for(unsigned i = 0; i < dirFound.size(); i++) {
             if(!dirFound.empty()) {
-                newString = dirFound.at(i);
-                //cout << newString << endl;
-                actDir.push(newString);
+                actDir.push(dirFound.at(i));
             } else {
                 break;
             }
